8082c91d96993e03dc680d7cdba19568f770c213
Dylan <720dylan@gmail.com>
2024-09-13 20:04:32 -0400

    Added Internship Section, filled in personal info for hero section and about section, added syling additions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
+// import sliderEffect from "./scripts/slider.js";
+
+
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+// Call the slider effect on the desired slider
+// document.addEventListener('DOMContentLoaded', () => {
+//     sliderEffect('.slider');
+// });
